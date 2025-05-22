28d536f4f2b9883052ccb5f2bf1ba1aa20dd6ab3
Thomas Petz <tompetz2000@gmail.com>
2025-05-22 06:29:03 -0400

    Updated state to use current value

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
